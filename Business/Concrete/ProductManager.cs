3ca81cc0fe4d42f3cb2632d9dd24134ec8a2e663
Emirhan Dogandemir <emirhandgndmr@mail.com.tr>
2021-02-01 19:53:38 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDao _productDao;
+
+        public ProductManager(IProductDao productDao)
+        {
+            _productDao = productDao;
+        }
+        public List<Product> GetAll()
+        {
+          // iş kodlari
+          // Yetkisi var mmı ?
+
+          return _productDao.GetAll();
+
+
+        }
+    }
+}
---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    //Çıplak Class kalmasın
+    public class Category : IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
