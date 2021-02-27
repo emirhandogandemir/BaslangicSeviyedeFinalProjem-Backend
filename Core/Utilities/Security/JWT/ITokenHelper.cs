c69fdb6a76395491cd82763df4590e8fb7de20c5
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2021-02-27 22:54:05 +0300

    27 şubat Security 2

---
+++ Core/Utilities/Security/JWT/ITokenHelper.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.JWT
+{
+    //test amaçlı bir token vermek isteyebiliriz , yada farklı bir araçla kullanmak isteyebiliriz diye interface
+  public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaim);
+
+
+
+    }
+}
