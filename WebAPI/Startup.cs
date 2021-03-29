546a13ae756ec3275714bf5006a3b7e2878839a1
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2021-03-29 15:46:33 +0300

    MiddleWare Added

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -74,6 +74,9 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.ConfigureCustomExceptionMiddleware();
+
             app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
